b32d09f2f76c6fd95d86b045057753a16105758f
Argus Duong <mrsugarvn@gmail.com>
2020-11-26 17:09:33 +0700

    Able to filter the latest URL by seconds

---
+++ sitemap.js
@@ -0,0 +1,36 @@
+Object.filter = (obj, predicate) =>
+    Object.keys(obj)
+        .filter(key => predicate(obj[key]))
+        .reduce((res, key) => (res[key] = obj[key], res), {});
+
+class Sitemap {
+    constructor() {
+        this.urls = [];
+    }
+
+    addURL(url, lastMod) {
+        this.urls[url] = [];
+        this.urls[url].lastMod = lastMod;
+        this.urls[url].images = [];
+    }
+
+    addImage(url, image) {
+        this.urls[url].images.push(image);
+    }
+
+    get list() {
+        return this.urls;
+    }
+
+    urlFilter(seconds) {
+        return Object.filter(this.urls, url => Math.round(Date.now() / 1000) -
+            url.lastMod < seconds)
+    }
+
+    toTimestamp(strDate) {
+        let datum = Date.parse(strDate);
+        return datum / 1000;
+    }
+}
+
+module.exports = Sitemap;
