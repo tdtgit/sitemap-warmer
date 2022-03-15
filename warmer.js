6c8670a23940dacde6e16bff35ce551952a71505
Robert Michalski <robert@surveylegend.com>
2022-03-15 23:14:27 +0100

    Fix undefined var

--- warmer.js
+++ warmer.js
@@ -127,7 +127,7 @@ export default class Warmer {
         if (this.settings.warmup_css === false && this.settings.warmup_js === false) {
             return
         }
-        if (accept_encoding !== 'deflate') {
+        if (headers.accept_encoding !== 'deflate') {
             return
         }
 
